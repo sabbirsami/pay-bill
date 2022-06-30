27fa483e751b31777b9a75458acfe009d79da0e4
SabbirSami <smd71430@gmail.com>
2022-06-30 10:52:55 +0600

    add inputs in modal

--- src/Components/DataTable.js
+++ src/Components/DataTable.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "react-bootstrap/Table";
 import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+
 const DataTable = () => {
+    const values = [true, "xxl-down"];
+    const [fullscreen, setFullscreen] = useState(true);
+    const [show, setShow] = useState(false);
+
+    function handleShow(breakpoint) {
+        setFullscreen(breakpoint);
+        setShow(true);
+    }
     return (
         <section>
             <div className="container py-4">
@@ -19,12 +30,76 @@ const DataTable = () => {
                             />
                         </div>
                         <div className="col-lg-4 text-end">
-                            <button className="btn btn-success border-0 bg-gradient rounded-0 px-4">
+                            <button
+                                key={values}
+                                onClick={() => handleShow(values)}
+                                className="btn btn-success border-0 bg-gradient rounded-0 px-4"
+                            >
                                 Add New Bill
                             </button>
                         </div>
                     </div>
                 </div>
+
+                {/* ADD NEW BILL MODAL */}
+
+                <Modal
+                    show={show}
+                    fullscreen={fullscreen}
+                    onHide={() => setShow(false)}
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title>Add New Bill</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className="col-lg-5 mx-auto justify-content-center align-items-center">
+                            <div className=" p-5">
+                                <Form>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Full Name</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            placeholder=""
+                                            className="rounded-0"
+                                        />
+                                    </Form.Group>
+                                    <Form.Group
+                                        className="mb-3"
+                                        controlId="formBasicEmail"
+                                    >
+                                        <Form.Label>Email address</Form.Label>
+                                        <Form.Control
+                                            type="email"
+                                            className="rounded-0"
+                                        />
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Phone Number</Form.Label>
+                                        <Form.Control
+                                            className="rounded-0"
+                                            type="number"
+                                        />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3">
+                                        <Form.Label>Paid Amount</Form.Label>
+                                        <Form.Control
+                                            className="rounded-0"
+                                            type="number"
+                                        />
+                                    </Form.Group>
+
+                                    <Button
+                                        className="rounded-0 btn btn-success border-0gb-gradient px-5"
+                                        type="submit"
+                                    >
+                                        Add Bill
+                                    </Button>
+                                </Form>
+                            </div>
+                        </div>
+                    </Modal.Body>
+                </Modal>
 
                 {/* DATA TABLE SECTION  */}
                 <Table bordered hover>
