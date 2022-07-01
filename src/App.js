c65b70cedd0a82e30b8c752f7baf4fcf3511306b
SabbirSami <smd71430@gmail.com>
2022-07-01 17:58:11 +0600

    add 404 page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SignUp from "./Components/SignUp";
 import RequireAuth from "./Components/RequireAuth";
 import Login from "./Components/Login";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import PageNotFount from "./Components/PageNotFount";
 
 const queryClient = new QueryClient();
 function App() {
@@ -24,6 +25,7 @@ function App() {
                     ></Route>
                     <Route path="/registration" element={<SignUp />}></Route>
                     <Route path="/" element={<Login />}></Route>
+                    <Route path="*" element={<PageNotFount />}></Route>
                 </Routes>
                 <Toaster />;
             </div>
