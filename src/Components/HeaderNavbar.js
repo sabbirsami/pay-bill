6e9f7d115a931b66825f768a4859ce47878153fd
Jochem Groeneweg <jochem.groeneweg@outlook.com>
2022-07-01 16:32:56 +0200

    The 'total paid' amount is now too formatted correctly

--- src/Components/HeaderNavbar.js
+++ src/Components/HeaderNavbar.js
@@ -4,34 +4,42 @@ import Container from "react-bootstrap/Container";
 import { useQuery } from "react-query";
 import Loading from "./Loading";
 
+const formatDollars = (number) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(number);
+};
+
 const HeaderNavbar = () => {
-    const { isLoading, error, data } = useQuery(["amount"], () =>
-        fetch("https://pay-bill-2022.herokuapp.com/bill-list").then((res) =>
-            res.json()
-        )
-    );
+  const { isLoading, error, data } = useQuery(["amount"], () =>
+    fetch("https://pay-bill-2022.herokuapp.com/bill-list").then((res) =>
+      res.json()
+    )
+  );
 
-    if (isLoading) {
-        return <Loading></Loading>;
-    }
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
 
-    return (
-        <header className="border-bottom">
-            <Navbar>
-                <Container>
-                    <Navbar.Brand className="" href="/billing">
-                        <i className="fs-2">Pay Bill</i>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                    <Navbar.Collapse className="justify-content-end">
-                        <Navbar.Text className="text-dark fs-5 ">
-                            Total Paid: <p className="d-inline">{data.total}</p>
-                        </Navbar.Text>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </header>
-    );
+  return (
+    <header className="border-bottom">
+      <Navbar>
+        <Container>
+          <Navbar.Brand className="" href="/billing">
+            <i className="fs-2">Pay Bill</i>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text className="text-dark fs-5 ">
+              Total Paid:{" "}
+              <p className="d-inline">{formatDollars(data.total)}</p>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default HeaderNavbar;
